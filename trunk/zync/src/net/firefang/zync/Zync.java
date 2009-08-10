2679cc1c21e312fb136d317f3caa4a354df660ab
omry <omry@70fe1234-81f5-460d-826b-5206857fec2e>
2009-08-10 21:53:11 +0000

    formated

--- trunk/zync/src/net/firefang/zync/Zync.java
+++ trunk/zync/src/net/firefang/zync/Zync.java
@@ -1,6 +1,5 @@
 package net.firefang.zync;
 
-
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -20,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 import net.firefang.swush.Swush;
+
 /**
  * @author omry
  */
@@ -28,150 +28,156 @@ public class Zync
 	public static void main(String[] args) throws Exception
 	{
 		CmdLineParser p = new CmdLineParser();
-		p.addStringOption('f',"file");
+		p.addStringOption('f', "file");
 		p.addBooleanOption('v', "verbose");
 		p.parse(args);
-		
-		File file = new File((String)p.getOptionValue("file", "backup.conf"));
-		boolean verbose = (Boolean)p.getOptionValue("verbose", false);
-		
-		
+
+		File file = new File((String) p.getOptionValue("file", "backup.conf"));
+		boolean verbose = (Boolean) p.getOptionValue("verbose", false);
+
 		Swush conf = new Swush(file);
-		
-		String rsync = conf.selectProperty("zync.rsync.command", "/usr/bin/rsync");
+
+		String rsync = conf.selectProperty("zync.rsync.command",
+				"/usr/bin/rsync");
 		String options[] = conf.selectFirst("zync.rsync.options").asArray();
 		if (options.length == 0) // no options specify, use default options
 		{
-			options = new String[]{"-a","--force","--delete-excluded","--delete", "--inplace"};
+			options = new String[]{"-a", "--force", "--delete-excluded", "--delete","--inplace"};
 		}
 		String globalDestination = conf.selectProperty("zync.rsync.destination");
 		List<Swush> backups = conf.select("zync.backup");
 		if (backups.size() > 0)
 		{
-			for(Swush backup : backups)
+			for (Swush backup : backups)
 			{
 				Rsync rs = new Rsync(rsync);
 				rs.setVerbose(verbose);
-				rs.setDest(backup.selectProperty("backup.destination", globalDestination));
+				rs.setDest(backup.selectProperty("backup.destination",globalDestination));
 				Swush optOverride = backup.selectFirst("backup.options");
 				if (optOverride != null)
 				{
 					options = optOverride.asArray();
 				}
 				rs.setOptions(options);
-				
+
 				rs.setHost(backup.selectProperty("backup.host"));
-				
+
 				List<String> dirs = new ArrayList<String>();
-				for(Swush dir : backup.select("backup.directory"))
+				for (Swush dir : backup.select("backup.directory"))
 				{
 					if (dir.isPair())
 						dirs.add(dir.getTextValue());
 					else
 					{
 						String[] ar = dir.asArray();
-						for(String s : ar) 
+						for (String s : ar)
 							dirs.add(s);
 					}
 				}
-				
+
 				List<String> excludes = new ArrayList<String>();
-				for(Swush exclude : backup.select("backup.exclude"))
+				for (Swush exclude : backup.select("backup.exclude"))
 				{
 					if (exclude.isPair())
 						excludes.add(exclude.getTextValue());
 					else
 					{
 						String[] ar = exclude.asArray();
-						for(String s : ar) 
+						for (String s : ar)
 							excludes.add(s);
 					}
 				}
-				
+
 				rs.setExcludes(excludes);
 				rs.copyDirs(dirs);
-				
+
 				rs.execute();
 			}
-			
+
 			snapshot(verbose, conf);
-		}
-		else
+		} else
 		{
 			System.err.println("no backup elements defined in " + file);
 		}
 	}
 
-	private static void snapshot(boolean verbose, Swush conf) throws IOException, InterruptedException, ParseException
+	private static void snapshot(boolean verbose, Swush conf)
+			throws IOException, InterruptedException, ParseException
 	{
 		String zfs = conf.selectProperty("zync.zfs.zfs", "/usr/sbin/zfs");
 		String zfsfs = conf.selectProperty("zync.zfs.backup_file_system");
-		DateFormat df = new SimpleDateFormat(conf.selectProperty("zync.zfs.snapshot.name_pattern", "yyyy_MM_dd__kk_mm_ss_zzz"));
+		DateFormat df = new SimpleDateFormat(conf.selectProperty(
+				"zync.zfs.snapshot.name_pattern", "yyyy_MM_dd__kk_mm_ss_zzz"));
 		String timestamp = df.format(new Date());
-		
+
 		List<String> c = new ArrayList<String>();
 		c.add(zfs);
 		c.add("snapshot");
 		c.add(zfsfs + "@" + timestamp);
-		
+
 		runProcess(c, System.out, verbose);
-        
-        String deleteOlder = conf.selectProperty("zync.zfs.snapshot.delete_older");
-        if (deleteOlder != null)
-        {
-        	long olderThen = 0;
-        	
-        	char u = deleteOlder.charAt(deleteOlder.length() - 1);
-        	float f = Float.parseFloat(deleteOlder.substring(0, deleteOlder.length() - 1));
-        	String msg;
-        	switch(u)
-        	{
-        	case 'm':
-        		olderThen = (long)(f * 1000 * 60);
-        		msg = "older than " + f + " minutes";
-        		break;
-        	case 'h':
-        		olderThen = (long)(f * 1000 * 60 * 60);
-        		msg = "older than " + f + " hours";
-        		break;
-        	case 'd':
-        		olderThen = (long)(f * 1000 * 60 * 60 * 24);
-        		msg = "older than " + f + " days";
-        		break;
-        	default:
-        		throw new RuntimeException("Unsupported unit type " + u + ", supported units are m : minute | h : hour | d : day");
-        	}
-        	
-        	olderThen = System.currentTimeMillis() - olderThen;
-        	
-        	if (verbose)
-        	{
-        		System.out.println("Deleting snapshots " + msg);
-        	}
-        	
-        	
-        	Map<Long, String> creation = getCreationTimes(zfs, zfsfs, verbose);
-        	List<Long> times = new ArrayList<Long>(creation.keySet());
-        	Collections.sort(times);
-        	for(long l : times)
-        	{
-        		if (l < olderThen)
-        		{
-        			c = new ArrayList<String>();
-        			c.add(zfs);
-        			c.add("destroy");
-        			c.add(creation.get(l));
-        			runProcess(c, System.out, verbose);
-        		}
-        	}
-        }
+
+		String deleteOlder = conf
+				.selectProperty("zync.zfs.snapshot.delete_older");
+		if (deleteOlder != null)
+		{
+			long olderThen = 0;
+
+			char u = deleteOlder.charAt(deleteOlder.length() - 1);
+			float f = Float.parseFloat(deleteOlder.substring(0, deleteOlder
+					.length() - 1));
+			String msg;
+			switch (u)
+			{
+			case 'm':
+				olderThen = (long) (f * 1000 * 60);
+				msg = "older than " + f + " minutes";
+				break;
+			case 'h':
+				olderThen = (long) (f * 1000 * 60 * 60);
+				msg = "older than " + f + " hours";
+				break;
+			case 'd':
+				olderThen = (long) (f * 1000 * 60 * 60 * 24);
+				msg = "older than " + f + " days";
+				break;
+			default:
+				throw new RuntimeException(
+						"Unsupported unit type "
+								+ u
+								+ ", supported units are m : minute | h : hour | d : day");
+			}
+
+			olderThen = System.currentTimeMillis() - olderThen;
+
+			if (verbose)
+			{
+				System.out.println("Deleting snapshots " + msg);
+			}
+
+			Map<Long, String> creation = getCreationTimes(zfs, zfsfs, verbose);
+			List<Long> times = new ArrayList<Long>(creation.keySet());
+			Collections.sort(times);
+			for (long l : times)
+			{
+				if (l < olderThen)
+				{
+					c = new ArrayList<String>();
+					c.add(zfs);
+					c.add("destroy");
+					c.add(creation.get(l));
+					runProcess(c, System.out, verbose);
+				}
+			}
+		}
 	}
-	
-	
-	private static Map<Long, String> getCreationTimes(String zfs, String zfsfs, boolean verbose) throws IOException, InterruptedException, ParseException
+
+	private static Map<Long, String> getCreationTimes(String zfs, String zfsfs,
+			boolean verbose) throws IOException, InterruptedException,
+			ParseException
 	{
-		//zfs list -o name,creation  -rHt snapshot storage/backup
-		
+		// zfs list -o name,creation -rHt snapshot storage/backup
+
 		List<String> c = new ArrayList<String>();
 		c.add(zfs);
 		c.add("list");
@@ -180,46 +186,45 @@ public class Zync
 		c.add("-rHt");
 		c.add("snapshot");
 		c.add(zfsfs);
-		
-		
+
 		ByteArrayOutputStream bout = new ByteArrayOutputStream();
 		runProcess(c, bout, false);
-        
-		BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bout.toByteArray())));
-        String line;
-        // Sat Aug  8 13:52 2009
-        SimpleDateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm yyyy");
-        
-        Map<Long,String> m = new HashMap<Long, String>();
-        while ((line = br.readLine()) != null)
-        {
-        	int i = line.indexOf('\t');
-        	String name = line.substring(0, i);
-        	String date = line.substring(i + 1);
-        	Date d = df.parse(date);
-        	m.put(d.getTime(), name);
-        }
-        
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				new ByteArrayInputStream(bout.toByteArray())));
+		String line;
+		// Sat Aug 8 13:52 2009
+		SimpleDateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm yyyy");
+
+		Map<Long, String> m = new HashMap<Long, String>();
+		while ((line = br.readLine()) != null)
+		{
+			int i = line.indexOf('\t');
+			String name = line.substring(0, i);
+			String date = line.substring(i + 1);
+			Date d = df.parse(date);
+			m.put(d.getTime(), name);
+		}
+
 		return m;
 	}
 
 	static String toString(List<String> commands)
 	{
 		String cmd = "";
-		for(String c : commands) 
+		for (String c : commands)
 		{
 			if (cmd.length() == 0)
 			{
 				cmd = c;
-			}
-			else
+			} else
 				cmd += " " + c;
 		}
 		return cmd;
 	}
-	
-	
-	public static void runProcess(List<String> commands, OutputStream out, boolean verbose) throws InterruptedException, IOException
+
+	public static void runProcess(List<String> commands, OutputStream out,
+			boolean verbose) throws InterruptedException, IOException
 	{
 		if (verbose)
 		{
@@ -227,40 +232,46 @@ public class Zync
 		}
 
 		ProcessBuilder pb = new ProcessBuilder(commands);
-		
-        Process process = pb.start();
-        InputStreamSucker stdout = new InputStreamSucker(process.getInputStream(), out);
-        InputStreamSucker stderr = new InputStreamSucker(process.getErrorStream(), System.err);
 
-        process.waitFor();
-        stdout.join();
-        stderr.join();
-        
-        int exit = process.exitValue();
-        if (exit != 0)
-        {
-        	System.err.println("Exit code " + exit + " from : " + toString(commands));
-        	System.exit(exit);
-        }
+		Process process = pb.start();
+		InputStreamSucker stdout = new InputStreamSucker(process.getInputStream(), out);
+		InputStreamSucker stderr = new InputStreamSucker(process.getErrorStream(), System.err);
+
+		process.waitFor();
+		stdout.join();
+		stderr.join();
+
+		int exit = process.exitValue();
+		if (exit != 0)
+		{
+			System.err.println("Exit code " + exit + " from : "
+					+ toString(commands));
+			System.exit(exit);
+		}
 	}
 }
-
 
 class Rsync
 {
 	private final String m_rsync;
+
 	private String m_host = "";
+
 	private List<String> m_dirs = new ArrayList<String>();
+
 	private List<String> m_excludes = new ArrayList<String>();
-	private String m_dest  = "";
+
+	private String m_dest = "";
+
 	private String m_options[];
+
 	private boolean m_verbose;
 
 	public Rsync(String rsync)
 	{
 		m_rsync = rsync;
 	}
-	
+
 	public void setVerbose(boolean verbose)
 	{
 		m_verbose = verbose;
@@ -269,68 +280,67 @@ class Rsync
 	public void setOptions(String options[])
 	{
 		m_options = options;
-		
+
 	}
-	
+
 	public void setHost(String host)
 	{
 		m_host = host;
 	}
-	
+
 	public void copyDirs(List<String> dirs)
 	{
 		m_dirs = dirs;
 	}
-	
+
 	public void setExcludes(List<String> excludes)
 	{
 		m_excludes = excludes;
 	}
-	
-	
+
 	public void setDest(String dest)
 	{
 		m_dest = dest;
 	}
-	
-	
+
 	public void execute() throws InterruptedException, IOException
 	{
 		List<String> commands = new ArrayList<String>();
 		commands.add(m_rsync);
-		if (m_verbose) commands.add("-v");
-		for(String opt : m_options) commands.add(opt);
-		for(String dir : m_dirs)
+		if (m_verbose)
+			commands.add("-v");
+		for (String opt : m_options)
+			commands.add(opt);
+		for (String dir : m_dirs)
 		{
-			commands.add (m_host + ":" + dir);
+			commands.add(m_host + ":" + dir);
 		}
-		
-		for(String exclude : m_excludes)
-		{
-			commands.add ("--exclude=" + exclude);
-		}
-		
 
-		String dst = replaceWord(m_dest , "${host}", m_host);
+		for (String exclude : m_excludes)
+		{
+			commands.add("--exclude=" + exclude);
+		}
+
+		String dst = replaceWord(m_dest, "${host}", m_host);
 		commands.add(dst);
 		new File(dst).mkdirs();
-		
-        ProcessBuilder pb = new ProcessBuilder(commands);
-        if (m_verbose)
-        {
-        	System.out.println(Zync.toString(commands));
-        }
-        Process process = pb.start();
-        InputStreamSucker stdout = new InputStreamSucker(process.getInputStream(), System.out);
-        InputStreamSucker stderr = new InputStreamSucker(process.getErrorStream(), System.err);
 
-        process.waitFor();
-        stdout.join();
-        stderr.join();
-        
-        int exit = process.exitValue();
-        if (exit != 0)
-        	System.exit(exit);
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		if (m_verbose)
+		{
+			System.out.println(Zync.toString(commands));
+		}
+		Process process = pb.start();
+		InputStreamSucker stdout = new InputStreamSucker(process.getInputStream(), System.out);
+		InputStreamSucker stderr = new InputStreamSucker(process.getErrorStream(), System.err);
+
+		process.waitFor();
+		stdout.join();
+		stderr.join();
+
+		int exit = process.exitValue();
+		if (exit != 0)
+			System.exit(exit);
 	}
 
 	static String replaceWord(String original, String find, String replacement)
@@ -351,6 +361,7 @@ class Rsync
 class InputStreamSucker extends Thread
 {
 	private final InputStream m_in;
+
 	private final OutputStream m_out;
 
 	public InputStreamSucker(InputStream in, OutputStream out)
@@ -359,20 +370,19 @@ class InputStreamSucker extends Thread
 		m_out = out;
 		start();
 	}
-	
-    public void run()
-    {
-        try
-        {
-            int c;
-            while ((c = m_in.read()) != -1)
-            {
-            	m_out.write((char)c);
-            }
-        }
-        catch (IOException e)
-        {
-        	e.printStackTrace();
-        }
-    }
+
+	public void run()
+	{
+		try
+		{
+			int c;
+			while ((c = m_in.read()) != -1)
+			{
+				m_out.write((char) c);
+			}
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
